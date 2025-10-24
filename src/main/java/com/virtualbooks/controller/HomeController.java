cc3df1f0b1eb3b798f4d2d0ee3ed9d5cea909fc5
RAmilG2006 <ricarditoguevara99@gmail.com>
2025-10-24 10:20:59 -0600

    about
    pues hice el about

--- src/main/java/com/virtualbooks/controller/HomeController.java
+++ src/main/java/com/virtualbooks/controller/HomeController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    // Ruta principal (página de inicio)
     @GetMapping("/")
     public String index() {
-        return "index"; // index.html dentro de templates
+        return "index"; // templates/index.html
+    }
+
+    // Ruta "about" (página de información)
+    @GetMapping("/about")
+    public String about() {
+        return "about"; // templates/about.html
     }
 }
